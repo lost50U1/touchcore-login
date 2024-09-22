b9ba211d3bd17646993a1ace425c748653262af7
Biruk Moges <birukmoges8@gmail.com>
2024-09-22 13:03:41 +0300

    ADDED logic to change the ui's based on the token
    - logic to change the ui's based on token authentication based on local storage for now
    - and imported necessary hooks for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
+
 import Login from "./components/Login";
 import SearchForm from "./components/SearchForm";
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      {/* <SearchForm /> */}
-      <Login />
+      {!token ? <Login setToken={setToken} /> : <SearchForm token={token} />}
     </main>
   );
 };
